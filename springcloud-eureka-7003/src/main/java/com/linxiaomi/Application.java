ed53c21c94b088412b59b95d50d59b2fcd2af0e8
linxiaomi <155294446@qq.com>
2019-01-23 18:19:29 +0800

    add springcloud-eureka-7003 eureka cluster

---
+++ springcloud-eureka-7003/src/main/java/com/linxiaomi/Application.java
@@ -0,0 +1,21 @@
+package com.linxiaomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author: admin
+ * @CreateDate: 2019/1/23 18:12
+ * @UpdateDate: 2019/1/23 18:12
+ * @Version: 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
