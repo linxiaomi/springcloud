531a47a4b53b026e7f398d6f7286806c572307d1
linxiaomi <155294446@qq.com>
2019-01-25 16:04:22 +0800

    add hystrix dashboard demo

---
+++ springcloud-consumer-hystrix-dashboard/src/main/java/com/linxiaomi/Application.java
@@ -0,0 +1,21 @@
+package com.linxiaomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Description:
+ * @Author: admin
+ * @CreateDate: 2019/1/25 16:00
+ * @UpdateDate: 2019/1/25 16:00
+ * @Version: 1.0
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
