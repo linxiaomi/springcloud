ea37891a242b53fb7c3feb8666ed00c059a198e9
linxiaomi <155294446@qq.com>
2019-01-27 17:30:31 +0800

    update ConfigBean.java add RandomRule

--- springcloud-consumer-dept-80/src/main/java/com/linxiaomi/cfgbeans/ConfigBean.java
+++ springcloud-consumer-dept-80/src/main/java/com/linxiaomi/cfgbeans/ConfigBean.java
@@ -2,6 +2,7 @@ package com.linxiaomi.cfgbeans;
 
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
 import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -53,7 +54,9 @@ public class ConfigBean {
     @Bean
     public IRule myRule() {
         // 达到的目的,用我们重新选择的随机算法代替默认的轮询
-        return new RandomRule();
+//        return new RandomRule();
+        // 重试
+        return new RetryRule();
 //        return new RoundRobinRule();
     }
 }
