2d3d1ea37cb78038688efc790104bcffda1d2885
linxiaomi <155294446@qq.com>
2019-01-27 18:30:42 +0800

    add userdefine rule

--- springcloud-consumer-dept-80/src/main/java/com/linxiaomi/Application.java
+++ springcloud-consumer-dept-80/src/main/java/com/linxiaomi/Application.java
@@ -1,8 +1,10 @@
 package com.linxiaomi;
 
+import com.zhlinxiaomi.config.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @Description:
@@ -13,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "SPRINGCLOUD-DEPT", configuration = MyRule.class)
 public class Application {
 
     public static void main(String[] args) {
---
+++ springcloud-consumer-dept-80/src/main/java/com/zhlinxiaomi/config/MyRule.java
@@ -0,0 +1,38 @@
+package com.zhlinxiaomi.config;
+
+import com.netflix.loadbalancer.IRule;
+//import com.netflix.loadbalancer.RetryRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: linxiaomi
+ * @CreateDate: 2019/1/27 17:38
+ * @UpdateDate: 2019/1/27 17:38
+ * @Version: 1.0
+ **/
+@Configuration
+public class MyRule {
+
+    /**
+     * @Description:    自定义访问规则(轮询、随机、加权重...)
+     * @Author:         linxiaomi
+     * @CreateDate:     2019/1/27 17:41
+     * @UpdateUser:     linxiaomi
+     * @UpdateDate:     2019/1/27 17:41
+     * @UpdateRemark:
+     * @Version:        1.0
+     * @return:
+     */
+    @Bean
+    public IRule myRule() {
+        return new MyRandomRule();
+        // 达到的目的,用我们重新选择的随机算法代替默认的轮询
+//        return new RandomRule();
+        // 重试
+//        return new RetryRule();
+//        return new RoundRobinRule();
+    }
+
+}
